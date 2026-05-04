76c66462757d2b5763d899a996771cdfd34f6e24
rahulrameshm0 <iamrahulm23@gmail.com>
2026-05-05 03:36:20 +0400

    designed the frontend section

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='posts', blank=True, null=True)
+    image = models.ImageField(upload_to='media/posts', blank=True, null=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
