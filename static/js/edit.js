f688a5d7f5156f74dc2d8f151a824d88930b41b2
rahulrameshm0 <iamrahulm23@gmail.com>
2026-05-13 18:51:53 +0400

    code updated

---
+++ static/js/edit.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll(".openModal");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        const modal = button.nextElementSibling;
+
+        modal.classList.toggle("show");
+    });
+});
